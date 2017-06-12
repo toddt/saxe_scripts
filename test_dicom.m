2ae93821582fa4db70bb33e40522c39caf28b656
toddt <toddt@mit.edu>
2017-06-12 18:16:19 -0400

    Delete test_dicom.m

--- test_dicom.m
+++
@@ -1,6 +0,0 @@
-study.name='fMRI_DataAnalysisTraining';
-study.path='/Users/toddt/fMRI_DataAnalysisTraining';
-study.subjects={'SAX_DAT_01'};
-
-%study = dicom_oneSub_twt(study)
-study = prep_oneSub_twt(study)
